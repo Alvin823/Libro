aebc46aaeb9e2a486bc3065075dabe8465f39826
Alvin <alvin8233@gmail.com>
2021-11-24 11:11:18 +0100

    1847.03 numeropaginas

--- Libro.java
+++ Libro.java
@@ -12,14 +12,16 @@ public class Libro {
 
     private String autor;
     private String titulo;
+    private int numeroPaginas;
 
     /**
      * Fija el autor y el titulo del libro a los dados como parametro
      */
-    public Libro(String autorLibro, String tituloLibro)
+    public Libro(String autorLibro, String tituloLibro, int numero)
     {
         autor = autorLibro;
         titulo = tituloLibro;
+        numeroPaginas = numero;
     }
     
     public String getAutor() {
@@ -28,5 +30,17 @@ public class Libro {
     
     public String getTitulo() {
         return titulo;
+    }
+    
+    public int getNumeroPaginas() {
+        return numeroPaginas;
+    }
+    
+    public void imprimeAutor() {
+        System.out.println(autor);
+    }
+    
+    public void imprimeTitulo() {
+        System.out.println(titulo);
     }
 }
